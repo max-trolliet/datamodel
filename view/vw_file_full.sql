5ec4f5d0f71b2f8bd1f4fcff9305166412d3a8a6
McSymus <maxime.trolliet@prilly.ch>
2018-04-09 11:29:59 +0200

    vw_file!
    new version with working update/insert functions

--- view/vw_file_full.sql
+++ view/vw_file_full.sql
@@ -1,19 +1,30 @@
-﻿-------------------------------------
--- view to Open/View an attached file
+﻿-- ******************************************************************************
+-- Open/view files in qgep
+-- ******************************************************************************
+--1. ADD fk_data_media : 
+-- ******************************************************************************
+
+-- Modification of table od_file --> insert fk_data_media
+/*
+ALTER TABLE qgep.od_file
+ADD COLUMN fk_data_media character varying(16);
+-- ******************************************************************************
+-- 2. qgep.vw_file : 
+-- ******************************************************************************
+
 -- View: qgep.vw_file
--------------------------------------
-
--- DROP VIEW qgep.vw_file;
-
+DROP VIEW qgep.vw_file;
+*/
 CREATE OR REPLACE VIEW qgep.vw_file AS 
  SELECT f.obj_id,
     f.identifier,
     f.kind AS file_kind,
     f.object,
     f.class,
-    dm.kind AS data_media_kind,
-    dm.path,
+    -- dm.path,   
     dm.path::text || f.path_relative::text AS _url,
+    f.fk_dataowner as dataowner,
+    f.fk_provider as provider,
     f.remark
    FROM qgep.od_file f
      LEFT JOIN qgep.od_data_media dm ON dm.obj_id::text = f.fk_data_media::text;
@@ -21,117 +32,11 @@ CREATE OR REPLACE VIEW qgep.vw_file AS
 ALTER TABLE qgep.vw_file
   OWNER TO postgres;
 
--- Trigger: vw_file_insert on qgep.vw_file
+-- ******************************************************************************
+-- 3. FUNCTIONS : 
+-- ******************************************************************************
 
--- DROP TRIGGER vw_file_insert ON qgep.vw_file;
-
-CREATE TRIGGER vw_file_insert
-  INSTEAD OF INSERT
-  ON qgep.vw_file
-  FOR EACH ROW
-  EXECUTE PROCEDURE qgep.vw_file_insert();
-
--- Trigger: vw_file_update on qgep.vw_file
-
--- DROP TRIGGER vw_file_update ON qgep.vw_file;
-
-CREATE TRIGGER vw_file_update
-  INSTEAD OF UPDATE
-  ON qgep.vw_file
-  FOR EACH ROW
-  EXECUTE PROCEDURE qgep.vw_file_update();
-
-  -- Trigger: vw_file_delete on qgep.vw_file
-
--- DROP TRIGGER vw_file_delete ON qgep.vw_file;
-
-CREATE TRIGGER vw_file_delete
-  INSTEAD OF DELETE
-  ON qgep.vw_file
-  FOR EACH ROW
-  EXECUTE PROCEDURE qgep.vw_file_delete();
-
-
------------------------------------
--- building INSERT
--- Function: vw_file_insert()
------------------------------------
-
-CREATE OR REPLACE FUNCTION qgep.vw_file_insert()
-  RETURNS trigger AS
-  
-$BODY$
-
-BEGIN
-  INSERT INTO  qgep.od_file(
-
-      obj_id,
-      class,
-      identifier,
-      kind,
-      object,
-      path_relative,
-      remark,
-      --last_modification,
-      --fk_dataowner,
-      --fk_provider,
-      fk_data_media
-      )
-  VALUES
-      (NEW.obj_id,
-      NEW.class,
-      NEW.identifier, 
-      NEW.file_kind,
-      NEW.object,
-      substring(NEW._url, char_length(NEW.path)+1,char_length(NEW._url)),
-      NEW.remark,
-      qgep.fk_data_media()
-      );
-  
-  RETURN NEW;
-END; $BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
-ALTER FUNCTION qgep.vw_file_insert()
-  OWNER TO postgres;
-
-
------------------------------------
--- building UPDATE
--- Function: qgep.vw_file_update()
------------------------------------
-
--- DROP FUNCTION qgep.vw_file_update();
-
-CREATE OR REPLACE FUNCTION qgep.vw_file_update()
-  RETURNS trigger AS
-$BODY$
-BEGIN
-  UPDATE  qgep.od_file
-    SET 
-      identifier = NEW.identifier, 
-      kind = NEW.file_kind,
-      class = NEW.class,
-      path_relative=NEW.substring(_url, char_length(path)+1,char_length(_url)),
-      remark=NEW.remark,
-      fk_data_media = qgep.fk_data_media();
-
-     --  (select(obj_id
--- 			from qgep.od_data_media
--- 			where od_data_media.path=new.path))
-  
-  RETURN NEW;
-END; $BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
-ALTER FUNCTION qgep.vw_file_update()
-  OWNER TO postgres;
-
------------------------------------
--- building DELETE
 -- Function: qgep.vw_file_delete()
------------------------------------
-
 -- DROP FUNCTION qgep.vw_file_delete();
 
 CREATE OR REPLACE FUNCTION qgep.vw_file_delete()
@@ -147,29 +52,128 @@ $BODY$
 ALTER FUNCTION qgep.vw_file_delete()
   OWNER TO postgres;
 
---------------------------------------------
--- building fk_data_media retrieve function
--- Function : ft_fk_data_media
---------------------------------------------
 
-CREATE OR REPLACE FUNCTION qgep.ft_fk_data_media()
+-- ******************************************************************************
+-- Function: qgep.vw_file_insert()
+-- DROP FUNCTION qgep.vw_file_insert();
+CREATE OR REPLACE FUNCTION qgep.vw_file_insert()
   RETURNS trigger AS
 $BODY$
 
-DECLARE
-	_fk character varying(16);
-
 BEGIN
 
-  SELECT obj_id INTO _fk
-  FROM qgep.od_data_media obj_id
-  WHERE NEW.path = od_data_media.path;
+NEW._url = replace(NEW._url, '\', '/');
 
-RETURN _fk;
+INSERT INTO qgep.od_file(
+        class,
+	identifier,
+	kind,
+	object,
+	path_relative,
+	fk_dataowner,
+	fk_provider,
+	fk_data_media,
+	remark)
 
-END;
+SELECT
+  NEW.class,
+  NEW.identifier, 
+  NEW.file_kind,
+  NEW.object,
+  SUBSTRING(NEW._url, LENGTH("path")+1, LENGTH(NEW._url)), -- path_relative,
+  NEW.dataowner, -- fk_dataowner,
+  NEW.provider, -- fk_provider,
+  obj_id, -- fk_data_media
+  NEW.remark  
+FROM qgep.od_data_media
+WHERE "path" = SUBSTRING(NEW._url FROM 1 FOR LENGTH("path"))
+ORDER BY LENGTH("path") DESC
+LIMIT 1;
 
+-- FOUND is a special variable which is always FALSE at the beginning of a PL/pgsql function and will be set by
+-- e.g. INSERT to TRUE if at least one row is affected.
+IF NOT FOUND THEN
+  RAISE WARNING 'Could not insert. File not in repository set in od_data_media!';
+END IF;
+
+  RETURN NEW;
+END; $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+ALTER FUNCTION qgep.vw_file_insert()
+  OWNER TO postgres;
+
+
+  -- ******************************************************************************
+-- Function: qgep.vw_file_update()
+-- DROP FUNCTION qgep.vw_file_update();
+
+CREATE OR REPLACE FUNCTION qgep.vw_file_update()
+  RETURNS trigger AS
 $BODY$
-  LANGUAGE plpgsql VOLATILE;
+BEGIN
+
+NEW._url = replace(NEW._url, '\', '/');
+
+  UPDATE  qgep.od_file
+    SET 
+    class = NEW.class,
+    identifier = NEW.identifier,
+    kind = NEW.file_kind,
+    object = NEW.object,
+    path_relative = SUBSTRING(NEW._url, LENGTH(dm.path)+1, LENGTH(NEW._url)), 
+    fk_dataowner = NEW.dataowner,
+    fk_provider = NEW.provider,
+    fk_data_media = dm.id, 
+    remark = NEW.remark
+
+FROM (
+  SELECT obj_id as id,
+	path
+	FROM qgep.od_data_media
+	WHERE path = SUBSTRING(NEW._url FROM 1 FOR LENGTH(path))
+	ORDER BY LENGTH(path) DESC
+	LIMIT 1)  dm
+
+WHERE obj_id = OLD.obj_id;
+
+     
+  RETURN NEW;
+END; 
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION qgep.vw_file_update()
+  OWNER TO postgres;
 
 
+-- TRIGGERS :
+-- ******************************************************************************
+-- Trigger: vw_file_delete on qgep.vw_file
+-- DROP TRIGGER vw_file_delete ON qgep.vw_file;
+
+CREATE TRIGGER vw_file_delete
+  INSTEAD OF DELETE
+  ON qgep.vw_file
+  FOR EACH ROW
+  EXECUTE PROCEDURE qgep.vw_file_delete();
+
+-- Trigger: vw_file_insert on qgep.vw_file
+-- DROP TRIGGER vw_file_insert ON qgep.vw_file;
+
+CREATE TRIGGER vw_file_insert
+  INSTEAD OF INSERT
+  ON qgep.vw_file
+  FOR EACH ROW
+  EXECUTE PROCEDURE qgep.vw_file_insert();
+
+-- Trigger: vw_file_update on qgep.vw_file
+-- DROP TRIGGER vw_file_update ON qgep.vw_file;
+
+CREATE TRIGGER vw_file_update
+  INSTEAD OF UPDATE
+  ON qgep.vw_file
+  FOR EACH ROW
+  EXECUTE PROCEDURE qgep.vw_file_update();
+-- ******************************************************************************
